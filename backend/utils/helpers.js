2c4e1e2e9dd476bdd1f987d07d73a59e430746d8
dangrous <5487802+dangrous@users.noreply.github.com>
2022-03-29 12:24:58 -0700

    add current value into portfolio

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,3 +1,11 @@
+const Song = require('../models/song')
+
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+
 const createPortfolio = async (trades) => {
   const ownedIds = []
   const soldIds = []
@@ -5,23 +13,43 @@ const createPortfolio = async (trades) => {
 
   trades.reverse()
 
-  trades.forEach(async (trade) => {
+  await asyncForEach(trades, async (trade) => {
     if (!ownedIds.includes(trade.song) && !soldIds.includes(trade.song)) {
       if (trade.action === 'BUY') {
         ownedIds.push(trade.song)
 
-        // let song = await Song.findOne({ spotifyId: trade.song })
+        let song = await Song.findOne({ spotifyId: trade.song })
+
+        console.log(song)
 
         songs.push({
           id: trade.song,
           purchasePrice: trade.price,
-          currentPrice: 45, // TODO NEED TO WORK ON ASYNC TO MAKE THIS REAL
+          currentPrice: song.currentPrice,
         })
       } else {
         soldIds.push(trade.song)
       }
     }
   })
+
+  // trades.forEach(async (trade) => {
+  //   if (!ownedIds.includes(trade.song) && !soldIds.includes(trade.song)) {
+  //     if (trade.action === 'BUY') {
+  //       ownedIds.push(trade.song)
+
+  //       // let song = await Song.findOne({ spotifyId: trade.song })
+
+  //       songs.push({
+  //         id: trade.song,
+  //         purchasePrice: trade.price,
+  //         currentPrice: 45, // TODO NEED TO WORK ON ASYNC TO MAKE THIS REAL
+  //       })
+  //     } else {
+  //       soldIds.push(trade.song)
+  //     }
+  //   }
+  // })
 
   return songs
 }
