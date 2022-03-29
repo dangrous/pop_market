c02efaaa2b07fadc6631f03a379193e76416c9d3
dangrous <5487802+dangrous@users.noreply.github.com>
2022-03-29 16:45:23 -0700

    get basic user info via oauth

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -6,6 +6,7 @@ const logger = require('../utils/logger')
 const config = require('../utils/config')
 const Song = require('../models/song')
 const helper = require('../utils/helpers')
+const axios = require('axios')
 
 loginRouter.post('/', async (request, response) => {
   const body = request.body
@@ -40,6 +41,48 @@ loginRouter.post('/', async (request, response) => {
     netWorth: 10,
     portfolio: songs,
   })
+})
+
+loginRouter.get('/oauth', (req, res) => {
+  res.redirect(
+    `https://accounts.spotify.com/authorize?response_type=code&client_id=${config.CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Flogin%2Fcallback`
+  )
+})
+
+loginRouter.get('/callback', async (req, res) => {
+  const code = req.query.code || null
+
+  const auth = Buffer.from(
+    `${config.CLIENT_ID}:${config.CLIENT_SECRET}`,
+    'utf8'
+  ).toString('base64')
+
+  const response = await axios
+    .post('https://accounts.spotify.com/api/token', null, {
+      params: {
+        code: code,
+        redirect_uri: 'http://localhost:3001/api/login/callback',
+        grant_type: 'authorization_code',
+      },
+      headers: {
+        Authorization: `Basic ${auth}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+    .catch((error) => {
+      logger.error(error)
+    })
+
+  const token = response.data.access_token
+
+  const user = await axios.get('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json',
+    },
+  })
+
+  res.send(user.data)
 })
 
 module.exports = loginRouter
