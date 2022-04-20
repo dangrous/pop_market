87aef9c809591996b14905a3dc942d8badbfc9bb
dangrous <5487802+dangrous@users.noreply.github.com>
2022-04-20 15:41:43 -0700

    add notification for loading menu

--- frontend/src/components/SongList.js
+++ frontend/src/components/SongList.js
@@ -47,71 +47,75 @@ const SongList = () => {
       </div>
 
       <ul className='list-group'>
-        {songs
-          ? songs.map((song, i) => {
-              const songData = song.track
+        {songs ? (
+          songs.map((song, i) => {
+            const songData = song.track
 
-              return (
-                <li className='list-group-item d-flex' key={songData.id}>
-                  <div className='col-xxl-auto fs-3 align-self-center'>
-                    {i + 1}.
-                    <img
-                      src={songData.album.images[1].url}
-                      alt={`Album cover art for "${songData.name}"`}
-                      height={94}
-                      className='mx-3'
-                    />
-                  </div>
-                  <div className='flex-grow-1 align-self-center'>
-                    <strong>{songData.name}</strong>
-                    <br />
-                    <em>
-                      {songData.artists.map((artist, i, artists) => {
-                        if (i !== artists.length - 1) {
-                          return artist.name + ', '
-                        } else {
-                          return artist.name
-                        }
-                      })}
-                    </em>
-                    <br />
-                    <a
-                      className='btn btn-sm btn-outline-dark'
-                      target='_blank'
-                      rel='noreferrer'
-                      href={songData.external_urls.spotify}
-                    >
-                      Listen on Spotify
-                    </a>
-                  </div>
-                  {user ? (
-                    user.songs.findIndex(
-                      (holding) => holding.song.spotifyId === songData.id
-                    ) === -1 ? (
-                      <div className='flex-shrink-0 align-self-center'>
-                        <button
-                          className='btn btn-primary btn-sm mt-2'
-                          onClick={() => buySong(songData.id, 100 - i)}
-                          disabled={user.points - (100 - i) < 0}
-                        >
-                          BUY This Song for {100 - i} Points!
-                        </button>
-                      </div>
-                    ) : (
-                      <div className='flex-shrink-0 align-self-center'>
-                        <button
-                          className='btn btn-info btn-sm mt-2'
-                          onClick={() => sellSong(songData.id, 100 - i)}
-                        >
-                          SELL This Song for {100 - i} Points!
-                        </button>
-                      </div>
-                    )
-                  ) : null}
-                </li>
-              )
-            })
-          : null}
+            return (
+              <li className='list-group-item d-flex' key={songData.id}>
+                <div className='col-xxl-auto fs-3 align-self-center'>
+                  {i + 1}.
+                  <img
+                    src={songData.album.images[1].url}
+                    alt={`Album cover art for "${songData.name}"`}
+                    height={94}
+                    className='mx-3'
+                  />
+                </div>
+                <div className='flex-grow-1 align-self-center'>
+                  <strong>{songData.name}</strong>
+                  <br />
+                  <em>
+                    {songData.artists.map((artist, i, artists) => {
+                      if (i !== artists.length - 1) {
+                        return artist.name + ', '
+                      } else {
+                        return artist.name
+                      }
+                    })}
+                  </em>
+                  <br />
+                  <a
+                    className='btn btn-sm btn-outline-dark'
+                    target='_blank'
+                    rel='noreferrer'
+                    href={songData.external_urls.spotify}
+                  >
+                    Listen on Spotify
+                  </a>
+                </div>
+                {user ? (
+                  user.songs.findIndex(
+                    (holding) => holding.song.spotifyId === songData.id
+                  ) === -1 ? (
+                    <div className='flex-shrink-0 align-self-center'>
+                      <button
+                        className='btn btn-primary btn-sm mt-2'
+                        onClick={() => buySong(songData.id, 100 - i)}
+                        disabled={user.points - (100 - i) < 0}
+                      >
+                        BUY This Song for {100 - i} Points!
+                      </button>
+                    </div>
+                  ) : (
+                    <div className='flex-shrink-0 align-self-center'>
+                      <button
+                        className='btn btn-info btn-sm mt-2'
+                        onClick={() => sellSong(songData.id, 100 - i)}
+                      >
+                        SELL This Song for {100 - i} Points!
+                      </button>
+                    </div>
+                  )
+                ) : null}
+              </li>
+            )
+          })
+        ) : (
+          <div class='alert alert-info' role='alert'>
+            We're loading the song list for today - this might take some time!
+          </div>
+        )}
       </ul>
     </div>
   )
