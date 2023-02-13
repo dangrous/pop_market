7ae690ccf0bef569de56d2becc54e50a7c42dfb2
dangrous <5487802+dangrous@users.noreply.github.com>
2023-02-12 22:58:29 -0500

    prep for fly.io deploy

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-const app = require('./app') // the actual Express application
-const http = require('http')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require('./app'); // the actual Express application
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})
+server.listen(8080, '0.0.0.0', () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
